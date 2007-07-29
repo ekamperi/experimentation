d5bc3d3d76a66a31c3391511396f82c81c435610
Stathis Kamperis <ekamperi@auth.gr>
2007-07-29 23:22:36 +0300

    Cosmetic changes  * Set indentation size to 4 spaces instead of 3  * Add space between operators  * Functions' opening brace starts _after_ their arguments' declarion

--- pthreads/pthread_join.c
+++ pthreads/pthread_join.c
@@ -7,27 +7,29 @@
 /* function prototypes */
 void *threadfun(void *arg);
 
-int main() {
-   pthread_t tid;   
-   int i, cnt[NUM_THREADS];
+int main(void)
+{
+    pthread_t tid;
+    int i, cnt[NUM_THREADS];
 
-   for (i=0; i<NUM_THREADS; i++) {
-      cnt[i] = i;
-      printf("Creating thread: %d\n", i);
+    for (i = 0; i < NUM_THREADS; i++) {
+        cnt[i] = i;
+        printf("Creating thread: %d\n", i);
       if (pthread_create(&tid, NULL, threadfun, (void *)&cnt[i])) {
-	 fprintf(stderr, "pthread_create() error\n");
-	 exit(EXIT_FAILURE);
+          fprintf(stderr, "pthread_create() error\n");
+          exit(EXIT_FAILURE);
       }
       if (pthread_join(tid, NULL)) {
-	 fprintf(stderr, "pthread_join() error\n");
-	 exit(EXIT_FAILURE);
+          fprintf(stderr, "pthread_join() error\n");
+          exit(EXIT_FAILURE);
       }
-   }   
+    }
 
    return EXIT_SUCCESS;
 }
 
-void *threadfun(void *arg) {   
-   printf("Hello! I am thread: %d\n", *(int *) arg);   
-   pthread_exit(NULL);
+void *threadfun(void *arg)
+{
+    printf("Hello! I am thread: %d\n", *(int *) arg);
+    pthread_exit(NULL);
 }
