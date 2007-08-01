be3e357f8fa7672f05d4bedfa4cf15d6240a6268
Stathis Kamperis <ekamperi@auth.gr>
2007-08-01 22:49:19 +0300

    Wait() for child to terminate

--- ipc/fork_execlp.c
+++ ipc/fork_execlp.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
     pid_t pid;
+    int retval;
 
     if ((pid = fork()) < 0) {  /* fork error */
         perror("fork");
         exit(EXIT_FAILURE);
     }
+
+    else if (pid > 0)          /* parent process */
+        wait(&retval);
 
     else if (pid == 0) {       /* child process */
         if (execlp("date", "date", (char *)0) < 0) {
