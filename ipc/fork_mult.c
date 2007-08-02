349fde9f8825d44af159503cfb59dc496b2bb820
Stathis Kamperis <ekamperi@auth.gr>
2007-08-02 16:40:06 +0300

    Initial import of fork_mult.c file

---
+++ ipc/fork_mult.c
@@ -0,0 +1,30 @@
+/* compile with:
+   gcc fork_mult.c -o fork_mult -Wall -W -Wextra -ansi -pedantic */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NUM_CHILDS 5
+
+int main(void)
+{
+    pid_t pid[NUM_CHILDS];
+    int i;
+
+    for (i = 0; i < NUM_CHILDS; i++) {
+        if ((pid[i] = fork()) < 0) {    /* fork errror */
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid[i] > 0) {          /* parent process */
+        }
+        else if (pid[i] == 0) {         /* child process */
+            printf("Child[%d] = %d\n", i, getpid());
+
+            /* break, or else every child will spawan it's own children */
+            break;
+        }
+    }
+   return EXIT_SUCCESS;
+}
