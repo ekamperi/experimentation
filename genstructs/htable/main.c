9066b36f1baed193e7041bcc991e51ff8991aa93
Stathis Kamperis <ekamperi@auth.gr>
2007-09-08 00:16:44 +0300

    ptable -> htable in main.c

---
+++ genstructs/htable/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "htable.h"
+
+u_int djb_hash(const void *key)
+{
+    /* DJB hashing */
+    u_int i, hash = 5381;
+
+    for (i = 0; i < strlen((char*)key); i++)
+        hash = ((hash << 5) + hash) + ((char*)key)[i];
+
+    return (hash & 0x7FFFFFFF);
+}
+
+int mycmp(const void *arg1, const void *arg2)
+{
+    return (strcmp((char *) arg1, (char *) arg2));
+}
+
+void myprintf(const void *key, const void *data)
+{
+    printf("%s(%s) ", (char *)key, (char *)data);
+}
+
+int main(void)
+{
+    htable_t htable;
+
+    /* Initialize table */
+    htable_init(&htable, 2<<5);
+
+    /* Setup callback functions */
+    htable.ht_hashf = djb_hash;
+    htable.ht_cmpf = mycmp;
+    htable.ht_printf = myprintf;
+
+    htable_insert(&htable, "stathis", "stathis");
+    htable_insert(&htable, "maria", "maria");
+    htable_insert(&htable, "kostas", "kostas");
+    htable_insert(&htable, "panagiotopoulos", "panagiotopoulos");
+    htable_insert(&htable, "eleni", "eleni");
+    htable_print(&htable);
+
+    htable_remove(&htable, "maria");
+    htable_print(&htable);
+
+    htable_free(&htable);
+
+    return EXIT_SUCCESS;
+}
