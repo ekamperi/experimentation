7d7847d9fca63150ea0f7335aa614a4cf0044eea
Stathis Kamperis <ekamperi@auth.gr>
2007-09-07 21:43:08 +0300

    Include arguments' names in function prototypes

--- genstructs/htable/htable.h
+++ genstructs/htable/htable.h
@@ -8,11 +8,11 @@ typedef struct hnode {
 } hnode_t;
 
 typedef struct htable {
-    size_t ht_size;
+    size_t ht_size;    /* size must be a power of 2 */
     u_int ht_used;
-    u_int (*ht_hashf)(const void *);
-    int (*ht_cmpf)(const void *, const void *);
-    void (*ht_printf)(const void *);
+    u_int (*ht_hashf)(const void *key);
+    int (*ht_cmpf)(const void *arg1, const void *arg2);
+    void (*ht_printf)(const void *key, const void *data);
     TAILQ_HEAD(htablehead, hnode) *ht_table;
 } htable_t;
 
@@ -21,4 +21,5 @@ void htable_init(htable_t *htable, size_t size);
 void htable_insert(htable_t *htable, const void *key, void *data);
 void *htable_search(const htable_t *htable, const void *key);
 void htable_print(const htable_t *htable);
+
 #endif    /* HTABLE_H */
