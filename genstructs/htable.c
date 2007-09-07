4beca3085b2f5ccfbd80e583c199ec87af1a8feb
Stathis Kamperis <ekamperi@auth.gr>
2007-09-07 19:00:16 +0300

    Split htable implementation to htable.c and htable.h files

--- genstructs/htable.c
+++ genstructs/htable.c
@@ -3,32 +3,14 @@
 #include <string.h>
 #include <sys/queue.h>
 
-typedef struct hnode {
-    const void *hn_key;
-    void *hn_data;
-    TAILQ_ENTRY(hnode) hn_next;
-} hnode_t;
-
-typedef struct htable {
-    size_t ht_size;
-    u_int ht_used;
-    u_int (*ht_hashf)(const void *);
-    int (*ht_cmpf)(const void *, const void *);
-    void (*ht_printf)(void *);
-    TAILQ_HEAD(htablehead, hnode) *ht_table;
-} htable_t;
-
-/* Function prototypes */
-void htable_init(htable_t *htable, size_t size);
-void htable_insert(htable_t *htable, const void *key);
-u_int htable_mkhash(const char *str);
+#include "htable.h"
 
 void htable_init(htable_t *htable, size_t size)
 {
     u_int i;
 
     /* Allocate memory for `size' tailq headers */
-    if ((htable->ht_table = malloc(size * sizeof(htable->ht_table))) == NULL) {
+    if ((htable->ht_table = malloc(size * sizeof *htable->ht_table)) == NULL) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
@@ -41,7 +23,7 @@ void htable_init(htable_t *htable, size_t size)
     htable->ht_used = 0;
 }
 
-void htable_insert(htable_t *htable, const void *key)
+void htable_insert(htable_t *htable, const void *key, void *data)
 {
     struct htablehead *phead;
     hnode_t *pnode;
@@ -50,10 +32,20 @@ void htable_insert(htable_t *htable, const void *key)
     /* Calculate hash */
     hash = htable->ht_hashf(key);
 
+    /* Search across chain if there is already an entry
+       with the same key. If there is, replace it. */
     phead = &htable->ht_table[hash & (htable->ht_size - 1)];
+    TAILQ_FOREACH(pnode, phead, hn_next)
+        if (htable->ht_cmpf(pnode->hn_key, key) == 0) {
+            pnode->hn_data = data;
+            return;
+        }
+
+    /* Allocate memory for new entry */
     if ((pnode = malloc(sizeof *pnode)) == NULL)
         return;
     pnode->hn_key = key;
+    pnode->hn_data = data;
 
     TAILQ_INSERT_TAIL(phead, pnode, hn_next);
     htable->ht_used++;
@@ -87,28 +79,44 @@ void htable_print(const htable_t *htable)
     }
 }
 
-u_int htable_mkhash(const char *str)
+u_int djb_hash(const void *str)
 {
     /* DJB hashing */
     u_int i, hash = 5381;
 
-    for (i = 0; i < strlen(str); i++)
-        hash = ((hash << 5) + hash) + str[i];
+    for (i = 0; i < strlen((char*)str); i++)
+        hash = ((hash << 5) + hash) + ((char*)str)[i];
 
     return (hash & 0x7FFFFFFF);
+}
+
+int mycmp(const void *arg1, const void *arg2)
+{
+    return (strcmp((char *) arg1, (char *) arg2));
+}
+
+void myprintf(const void *data)
+{
+    printf("%s ", (char *)data);
 }
 
 int main(void)
 {
     htable_t ptable;
 
+    /* Initialize table */
     htable_init(&ptable, 10);
 
-    htable_insert(&ptable, "stathis");
-    htable_insert(&ptable, "maria");
-    htable_insert(&ptable, "kostas");
-    htable_insert(&ptable, "panagiotopoulos");
-    htable_insert(&ptable, "eleni");
+    /* Setup callback functions */
+    ptable.ht_hashf = djb_hash;
+    ptable.ht_cmpf = mycmp;
+    ptable.ht_printf = myprintf;
+
+    htable_insert(&ptable, "stathis", "stathis");
+    htable_insert(&ptable, "maria", "maria");
+    htable_insert(&ptable, "kostas", "kostas");
+    htable_insert(&ptable, "panagiotopoulos", "panagiotopoulos");
+    htable_insert(&ptable, "eleni", "eleni");
 
     htable_print(&ptable);
 
---
+++ genstructs/htable.h
@@ -0,0 +1,24 @@
+#ifndef HTABLE_H
+#define HTABLE_H
+
+typedef struct hnode {
+    const void *hn_key;
+    void *hn_data;
+    TAILQ_ENTRY(hnode) hn_next;
+} hnode_t;
+
+typedef struct htable {
+    size_t ht_size;
+    u_int ht_used;
+    u_int (*ht_hashf)(const void *);
+    int (*ht_cmpf)(const void *, const void *);
+    void (*ht_printf)(const void *);
+    TAILQ_HEAD(htablehead, hnode) *ht_table;
+} htable_t;
+
+/* Function prototypes */
+void htable_init(htable_t *htable, size_t size);
+void htable_insert(htable_t *htable, const void *key, void *data);
+u_int djb_hash(const void *str);
+
+#endif    /* HTABLE_H */
