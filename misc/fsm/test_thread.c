482af1c87e4a1046f7c8aa2a23b2b76d506e5fbc
Stathis Kamperis <ekamperi@auth.gr>
2007-10-29 22:33:39 +0200

    Cleanup test case with threads

--- misc/fsm/test_thread.c
+++ misc/fsm/test_thread.c
@@ -1,22 +1,18 @@
-/* compile with:
-   gcc pthread_create.c -o pthread_create -lpthread -Wall -W -Wextra -ansi -pedantic */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
 
 #include "fsm.h"
 #include "states.h"
 #include "types.h"
 
 /* function prototypes */
-void *threadfun(void *arg);
+void *thread_char_gen(void *arg);
 void diep(const char *s);
 
 int main(void)
 {
-    fsm_t fsm;
+    fsm_t *fsm;
     pthread_t tid_char_gen;
 
     /* Initialize fsm */
