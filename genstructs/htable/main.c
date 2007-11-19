3f6f1f23380de576c90edf22a64a36788018fdba
Stathis Kamperis <ekamperi@auth.gr>
2007-11-19 12:53:15 +0200

    Build up a more robust test case

--- genstructs/htable/main.c
+++ genstructs/htable/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>    /* for time() in srand() */
 
 #include "htable.h"
 
-unsigned int djb_hash(const void *key)
+unsigned int myhashf(const void *key)
 {
     unsigned int i, hash = 5381;
     char *str = (char *)key;
@@ -12,12 +13,12 @@ unsigned int djb_hash(const void *key)
     for (i = 0; i < strlen(str); i++)
         hash = ((hash << 5) + hash) + str[i];
 
-    return (hash & 0x7FFFFFFF);
+    return hash;
 }
 
 int mystrcmp(const void *arg1, const void *arg2)
 {
-    return (strcmp((char *) arg1, (char *) arg2));
+    return (strcmp((char *)arg1, (char *)arg2));
 }
 
 void myprintf(const void *key, const void *data)
@@ -30,35 +31,51 @@ void print_elm(void *data)
     printf("%s\n", (char *)data);
 }
 
+void get_rand_string(char *str, size_t len)
+{
+    size_t i;
+
+    for (i = 0; i < len; i++)
+        str[i] = 32 + 65 + (rand() / (RAND_MAX / 26 + 1));
+    str[i] = '\0';
+}
+
 int main(void)
 {
     htable_t htable;
-    char *p = malloc(100);
-    char *s = malloc(100);
-    char *t = malloc(100);
-    char *q = malloc(100);
+    size_t i;
+    char str[20][10+1];    /* +1 for '\0' */
 
-    strcpy(p, "stathis");
-    strcpy(s, "maria");
-    strcpy(t, "kostas");
-    strcpy(q, "eleni");
+    /* Initialize random number generator */
+    srand(time(NULL));
+
+    for (i = 0; i < sizeof str / sizeof *str; i++)
+        get_rand_string(str[i], 10);
 
     /* Initialize table */
-    if (htable_init(&htable, 1, 1, djb_hash, mystrcmp, myprintf) == HT_NOMEM) {
+    if (htable_init(&htable, 32, 2, myhashf, mystrcmp, myprintf) == HT_NOMEM) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
 
-    htable_insert(&htable, "stathis", p);
-    htable_insert(&htable, "maria", s);
-    htable_insert(&htable, "kostas", t);
-    htable_insert(&htable, "eleni", q);
-    htable_print(&htable);
+    /* Add random entries in hash table */
+    for (i = 0; i < sizeof str / sizeof *str; i++)
+        htable_insert(&htable, str[i], str[i]);
+    /* htable_print(&htable); */
 
+    /* Traverse all elemets and print each one of them */
     htable_traverse(&htable, print_elm);
 
+    /* Print the length of every chain */
+    for (i = 0; i < htable_get_size(&htable); i++)
+        printf("chain[%lu] = %lu\n",
+               (unsigned long)i,
+               (unsigned long)htable_stat_get_chain_len(&htable, i));
+
+    /* Print number of automatic resizes */
+    printf("Automatic resizes: %lu\n", (unsigned long)htable_stat_get_grows(&htable));
+
     /* Free memory */
-    htable_free_all_obj(&htable, HT_FREEDATA);
     htable_free(&htable);
 
     return EXIT_SUCCESS;
