6b767b88cdfdb337f0b3e9680c4e5599c0b0dc84
Stathis Kamperis <ekamperi@auth.gr>
2008-01-01 20:05:02 +0200

    Remove whitespace, explicitly set initial capacity of array

--- proplib/prop1.c
+++ proplib/prop1.c
@@ -7,20 +7,23 @@ int main(int argc, char *argv[])
 	prop_array_t pa;
 	int i;
 
-	/* Create array object */
-	pa = prop_array_create();
+        /* Create array object with initial capacity 10
+         * Note that the array will expand on demand
+         * by the prop_array_add()
+        */
+	pa = prop_array_create(10);
 
-	/* For every argument, create a reference to it 
-	 * and store it in the array 
+	/* For every argument, create a reference to it
+	 * and store it in the array
 	 */
 	for (i = 0; i < argc; i++)
 		prop_array_add(pa, prop_string_create_cstring_nocopy(argv[i]));
 
 	/* Export array contents to file as XML */
 	prop_array_externalize_to_file(pa, "./data.xml");
-	
+
 	/* Release array object */
-	prop_object_release(pa);	
-	
-	return EXIT_SUCCESS; 
+	prop_object_release(pa);
+
+	return EXIT_SUCCESS;
 }
