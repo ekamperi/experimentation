40fdb5ef493ad73c710ceaa5bd29b8e373d47a41
Stathis Kamperis <ekamperi@gmail.com>
2008-01-04 13:20:46 +0100

    Fix indentation

--- proplib/prop1.c
+++ proplib/prop1.c
@@ -4,31 +4,31 @@
 
 int main(int argc, char *argv[])
 {
-        /* Declare a pointer to a prop_array object
-         * Note that prop_array_t is a pointer being
-         * hidden inside a typedef, i.e.
-         * typedef struct _prop_array *prop_array_t;
-         */
-	prop_array_t pa;
-	int i;
+    /* Declare a pointer to a prop_array object
+     * Note that prop_array_t is a pointer being
+     * hidden inside a typedef, i.e.
+     * typedef struct _prop_array *prop_array_t;
+     */
+    prop_array_t pa;
+    int i;
 
-        /* Create array object with initial capacity 10
-         * Note that the array will expand on demand
-         * by the prop_array_add()
-        */
-	pa = prop_array_create_with_capacity(10);
+    /* Create array object with initial capacity 10
+     * Note that the array will expand on demand
+     * by the prop_array_add()
+     */
+    pa = prop_array_create_with_capacity(10);
 
-	/* For every argument, create a reference to it
-	 * and store it in the array
-	 */
-	for (i = 0; i < argc; i++)
-		prop_array_add(pa, prop_string_create_cstring_nocopy(argv[i]));
+    /* For every argument, create a reference to it
+     * and store it in the array
+     */
+    for (i = 0; i < argc; i++)
+	prop_array_add(pa, prop_string_create_cstring_nocopy(argv[i]));
 
-	/* Export array contents to file as XML */
-	prop_array_externalize_to_file(pa, "./data.xml");
+    /* Export array contents to file as XML */
+    prop_array_externalize_to_file(pa, "./data.xml");
 
-	/* Release array object */
-	prop_object_release(pa);
+    /* Release array object */
+    prop_object_release(pa);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
