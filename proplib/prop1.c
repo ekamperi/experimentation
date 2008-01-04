f3908fe09aabd39c5d160ec83b938937242bbcc7
Stathis Kamperis <ekamperi@gmail.com>
2008-01-04 19:46:48 +0100

    Check return codes for failure

--- proplib/prop1.c
+++ proplib/prop1.c
@@ -1,3 +1,4 @@
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <prop/proplib.h>
@@ -10,6 +11,7 @@ int main(int argc, char *argv[])
      * typedef struct _prop_array *prop_array_t;
      */
     prop_array_t pa;
+    prop_string_t ps;
     int i;
 
     /* Create array object with initial capacity 10
@@ -17,15 +19,28 @@ int main(int argc, char *argv[])
      * by the prop_array_add()
      */
     pa = prop_array_create_with_capacity(10);
+    if (pa == NULL)
+        errx(EXIT_FAILURE, "prop_array_create_with_capacity() failed\n");
 
     /* For every argument, create a reference to it
      * and store it in the array
      */
-    for (i = 0; i < argc; i++)
-        prop_array_add(pa, prop_string_create_cstring_nocopy(argv[i]));
+    for (i = 0; i < argc; i++) {
+        ps = prop_string_create_cstring_nocopy(argv[i]);
+        if (ps == NULL) {
+            prop_object_release(pa);
+            errx(EXIT_FAILURE, "prop_string_create_cstring_nocopy() failed\n");
+        }
+
+        if (prop_array_add(pa, ps) == FALSE) {
+            prop_object_release(pa);
+            errx(EXIT_FAILURE, "prop_array_add() failed\n");
+        }
+    }
 
     /* Export array contents to file as XML */
-    prop_array_externalize_to_file(pa, "./data.xml");
+    if (prop_array_externalize_to_file(pa, "./data.xml") == FALSE)
+        errx(EXIT_FAILURE, "prop_array_externalize_to_file() failed\n");
 
     /* Release array object */
     prop_object_release(pa);
