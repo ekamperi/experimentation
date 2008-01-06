d05c61b00004c75877adf93b1a52f18dd5673c0f
Stathis Kamperis <ekamperi@gmail.com>
2008-01-06 21:10:01 +0100

    Add some comments and some whitespace for clarity

--- proplib/prop_expand.c
+++ proplib/prop_expand.c
@@ -20,16 +20,23 @@ int main(void)
     if (pa == NULL)
         errx(EXIT_FAILURE, "prop_array_create_with_capacity()");
 
+    /* Add up to `NUM_STRINGS' prop_string_t objects
+     * and watch how the array expands on demand
+     */
     for (i = 0; i < NUM_STRINGS; i++) {
+        /* Print statistics */
         printf("count = %u\tcapacity = %u\n",
                prop_array_count(pa),
                prop_array_capacity(pa));
+      
+        /* Create prop_string_t object */
         ps = prop_string_create_cstring_nocopy("test");
         if (ps == NULL) {
             prop_object_release(pa);
             errx(EXIT_FAILURE, "prop_string_create_cstring_nocopy()");
         }
 
+        /* Add object in array */
         if (prop_array_add(pa, ps) == FALSE) {
             prop_object_release(pa);
             errx(EXIT_FAILURE, "prop_array_add()");
