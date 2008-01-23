721fef010801d8237856c08c0bc85e1dc5221d0c
Stathis Kamperis <ekamperi@gmail.com>
2008-01-23 22:49:46 +0100

    First step towards completion

--- proplib/prop_du.c
+++ proplib/prop_du.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>    /* for waitpid() macros */
+#include <prop/proplib.h>
 
+#define INIT_CAPACITY 100
 #define MAX_STR 100
+#define MAX_TOKENS 3
 
 int main(void)
 {
     char str[MAX_STR];
-    int ret;
+    char *tokens[MAX_TOKENS];    /* for ``du'' output parse */
+    char *last, *p, *s;
+    int i, j, ret;
     FILE *fp;
+    prop_dictionary_t pd;
+    prop_string_t ps;
 
     /* Initiate pipe stream to ``du'' */
     fp = popen("du", "r");
@@ -17,10 +25,37 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    /* Create dictionary */
+    pd = prop_dictionary_create_with_capacity(INIT_CAPACITY);
+    if (pd == NULL) {
+        pclose(fp);
+        prop_object_release(pd);
+        errx(EXIT_FAILURE, "prop_dictionary_create_with_capacity()");
+    }
+
     /* Read from stream */
     while (fgets(str, MAX_STR, fp) != NULL) {
-        printf("%s", str);
+        /* Parse output of ``du'' command */
+        i = 0;
+        for ((p = strtok_r(str, "\t", &last)); p;
+             (p = strtok_r(NULL, "\t", &last)), i++) {
+            if (i < MAX_TOKENS - 1)
+                tokens[i] = p;
+        }
+        tokens[i] = NULL;
+
+        /* Trim '\n' from tokens[1] */
+        (tokens[1])[strlen(tokens[1]) - 1] = '\0';
+
+        ps = prop_string_create_cstring(tokens[0]);
+        prop_dictionary_set(pd, tokens[1], ps);
+        prop_object_release(ps);
+
+        printf("%s\n", s);
     }
+
+    prop_dictionary_externalize_to_file(pd, "./data.xml");
+    prop_object_release(pd);
 
     /* Close pipe stream */
     ret = pclose(fp);
