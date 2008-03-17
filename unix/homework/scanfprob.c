13f7cbad6cf41fa48fa72fb5d370ee545e294560
Stathis Kamperis <ekamperi@gmail.com>
2008-03-17 09:55:46 +0100

    Add some comments and references

--- unix/homework/scanfprob.c
+++ unix/homework/scanfprob.c
@@ -1,9 +1,29 @@
+/*
+ * Compile with:
+ * gcc scanfprob.c -o scanfprob -Wall -W -Wextra -ansi -pedantic
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
     int a, rv;
 
+    /*
+     * When scanf is attempting to convert numbers, any non-numeric characters
+     * it encounters terminate the conversion and are left in the input stream.
+     * Therefore, unexpected non-numeric input ``jams'' scanf again and again.
+     * E.g.
+     * 
+     * do {
+     *     printf("Input: ");
+     *     scanf("%d", &a);
+     * } while (a != -1);
+     * 
+     * See also: http://c-faq.com/stdio/scanfjam.html
+     */
+
+    /* The correct way to do it */
     do {
         printf("Input: ");
         rv = scanf("%d", &a);
