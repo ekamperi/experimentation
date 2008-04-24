546b073917ce7d8fe3c6f2db0d8545b0561e02a4
Stathis Kamperis <ekamperi@auth.gr>
2008-04-25 02:55:20 +0300

    Many changes to mybuild.sh script

--- cdev/mybuild.sh
+++ cdev/mybuild.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
+TESTDEV=/home/stathis/eleutheria/cdev
 # Copy dev files to netbsd source tree
-sudo cp -v mydev.c /usr/src/sys/dev
-sudo cp -v mydev.h /usr/src/sys/sys
+cp -v mydev.c /usr/src/sys/dev
+cp -v mydev.h /usr/src/sys/sys
 
 # Recompile kernel
 cd /usr/src
-sudo ./build.sh  -O ../obj -T ../tools -u kernel=MY_GENERIC || exit
-
-# Install kernel
-cd ../obj/sys/arch/i386/compile/MY_GENERIC/
-sudo make install
+./build.sh  -O ../obj -T ../tools -u kernel=XEN3_DOMU || exit
 
 # Build testdev
-cd ~/eleutheria/netbsd/cdev
-gcc testdev.c -lprop -I /usr/src/sys -Wall
+cd $TESTDEV
+pwd
+gcc testdev.c -o testdev -lprop -I /usr/src/sys -Wall
 
+# Shutdown domU if it's running
+domUid=`xm list | grep nbsd-dom2 | awk {'print $2'}`
+xm shutdown $domUid
+
+# Copy testdev in domU's virtual disk
+sleep 2
+vnconfig vnd0 /usr/xen/nbsd-disk || exit
+mount /dev/vnd0a /mnt
+cp /home/stathis/eleutheria/cdev/testdev /mnt/root
+umount /dev/vnd0a
+vnconfig -u vnd0
