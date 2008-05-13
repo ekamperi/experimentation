becc002682cf97b62df1fd9db34401a16bd20938
Stathis Kamperis <ekamperi@auth.gr>
2008-05-13 20:42:10 +0300

    Swap rows,cols

--- homework/2darray.c
+++ homework/2darray.c
@@ -6,8 +6,8 @@ void foo1(int **mat, int rows, int cols)
 {
     int i, j;
 
-    for (i = 0; i < cols; i++)
-        for (j = 0; j < rows; j++)
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
             printf("%d ", mat[i][j]);
     printf("\n");
 }
@@ -25,7 +25,7 @@ void foo2(int **mat, int rows, int cols)
     }
 
     for (i = 0; i < rows; i++) {
-        aux[i] = (int *)mat + rows * i;
+        aux[i] = (int *)mat + cols * i;
         for (j = 0; j < cols; j++)
             printf("%d ", aux[i][j]);
         printf("\n");
@@ -36,12 +36,13 @@ void foo2(int **mat, int rows, int cols)
 
 int main(void)
 {
-    int mat[3][3] = { {1, 2, 3},
-                      {4, 5, 6},
-                      {7, 8, 9} };
+    int mat[4][3] = { { 1,  2,  3},
+                      { 4,  5,  6},
+                      { 7,  8,  9},
+                      {10, 11, 12} };
 
-    foo2((int **)&mat[0][0], 3, 3);
-    foo1((int **)&mat[0][0], 3, 3);
+    foo2((int **)&mat[0][0], 4, 3);
+    foo1((int **)&mat[0][0], 4, 3);
 
     return EXIT_SUCCESS;
 }
