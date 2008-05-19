0952ad0f85c0ca00280711a63280ba9107ad42b4
Stathis Kamperis <ekamperi@auth.gr>
2008-05-20 00:30:17 +0300

    Add missing braces in for-loop

--- homework/2darray.c
+++ homework/2darray.c
@@ -6,10 +6,11 @@ void foo1(int **mat, int rows, int cols)
 {
     int i, j;
 
-    for (i = 0; i < rows; i++)
+    for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++)
             printf("%d ", mat[i][j]);
-    printf("\n");
+        printf("\n");
+    }
 }
 
 /* Correct */
