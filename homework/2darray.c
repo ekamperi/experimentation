78bd072e78097477cb2beebfd037d51ae2a8dcb8
Stathis Kamperis <ekamperi@auth.gr>
2008-05-20 00:32:31 +0300

    Add one more solution

--- homework/2darray.c
+++ homework/2darray.c
@@ -35,6 +35,18 @@ void foo2(int **mat, int rows, int cols)
     free(aux);
 }
 
+/* Correct */
+void foo3(int **mat, int rows, int cols)
+{
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++)
+            printf("%d ", *((int *)mat + cols * i) + j);
+        printf("\n");
+    }
+}
+
 int main(void)
 {
     int mat[4][3] = { { 1,  2,  3},
@@ -42,6 +54,7 @@ int main(void)
                       { 7,  8,  9},
                       {10, 11, 12} };
 
+    foo3((int **)&mat[0][0], 4, 3);
     foo2((int **)&mat[0][0], 4, 3);
     foo1((int **)&mat[0][0], 4, 3);
 
